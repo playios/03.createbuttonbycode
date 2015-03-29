6c41a16a729ab4e760503b698e3a8064863933af
Adiya <libera1986@hotmail.com>
2015-03-29 15:06:46 +0800

    功能完成

--- 03.代码创建按钮/DYViewController.m
+++ 03.代码创建按钮/DYViewController.m
@@ -17,7 +17,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    UIButton *button=[[UIButton alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    button.backgroundColor=[UIColor redColor];
+    [button setBackgroundImage:[UIImage imageNamed:@"btn_01"] forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [button setTitle:@"点我啊 "forState:UIControlStateNormal];
+    
+    
+       [button setBackgroundImage:[UIImage imageNamed:@"btn_02"] forState:UIControlStateHighlighted];
+    
+     [button setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+      [button setTitle:@"点我干啥"forState:UIControlStateHighlighted];
+    
+    button.contentVerticalAlignment=UIControlContentVerticalAlignmentBottom;
+    [self.view addSubview:button];
+
 }
 
 - (void)didReceiveMemoryWarning
